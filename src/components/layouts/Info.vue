afd6a7a18bb547649f2bff7057ee34676afe4827
LueKely <luekely43110@gmail.com>
2023-06-24 10:17:17 +0800

    Feat: Bulletin Board

--- src/components/layouts/Info.vue
+++ src/components/layouts/Info.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="w-100 h-100 pa-5">
     <slot name="header"></slot>
-    <div>
-      <slot name="info"></slot>
-      <slot name="tier"></slot>
+
+    <div class="d-flex">
+      <section class="d-flex flex-column mr-2">
+        <slot name="info"></slot>
+        <slot name="tier"></slot>
+      </section>
+      <section class="w-100 d-flex justify-center">
+        <slot name="bulletin"></slot>
+      </section>
     </div>
+
+    <div></div>
   </div>
 </template>
